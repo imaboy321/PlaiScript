49498386b282bb40b2bea91286420830fca06926
RedSoloFox <RedSoloFox@gmail.com>
2016-06-30 04:03:08 -0400

    Added basic HTML page downloading
    Currently only downloads pages from the pages.txt file inside the HTML
    directory

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 #Imports
-import sys, os, urllib2
+import sys, os, urllib, htmllib
 #Base Variables
 version = "0.0.1"
 versionName = "The Simple One"
 html_directory = "HTML"
 #Definitions
 def get_home_page():
-    urllib2.urlopen("https://github.com/Plailect/Guide/wiki")
+    urllib.urlretrieve("https://github.com/Plailect/Guide/wiki", html_directory+"/home_page.html")
+
+def get_all_pages():
+    page_text = open(html_directory+"/pages.txt",'r')
+    for link in page_text:
+        stripped_link = link.strip("\n")
+        urllib.urlretrieve(stripped_link, html_directory+"/"+stripped_link.strip("https://github.com/Plailect/Guide/wiki/Part-"+".html"))
+
 
 #Main
-get_home_page()
+get_all_pages()
 for html_file in os.listdir(html_directory):
     print html_file
 print version, "\nPress Enter To Exit"
-raw_input()
 sys.exit()
